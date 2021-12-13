f2944f3a1d7059e1ae166d3e20ed63a8f461ab33
Naimeless <65905724+Naimeless@users.noreply.github.com>
2021-12-13 17:42:36 +0300

    работа с ссылкой на contact-block (js)

---
+++ app/js/codes.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("a").click(function () {
+        let elementClick = $(this).attr("href");
+        let destination = $(elementClick).offset().top;
+    });
+});
