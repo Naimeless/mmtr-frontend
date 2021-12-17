ab29267bc089e36488a07c00fd3ddb0bac933b54
Naimeless <65905724+Naimeless@users.noreply.github.com>
2021-12-17 16:40:48 +0300

    ссылка на блок contact (js)
    Добавлен переход к блоку при нажатии на кнопку

---
+++ app/js/codes.js
@@ -0,0 +1,6 @@
+let contactFormLink = document.getElementById('contact-form-link');
+
+contactFormLink.onclick = function link() {
+    document.getElementById('contact-form').scrollIntoView({behavior: 'smooth'});
+  }
+debugger
