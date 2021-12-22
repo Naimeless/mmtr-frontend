b1020a8b0c73c9f42b9793474bcacfee81e0107c
Naimeless <65905724+Naimeless@users.noreply.github.com>
2021-12-22 16:56:01 +0300

    js
    добавление стилей

--- app/js/codes.js
+++ app/js/codes.js
@@ -3,4 +3,63 @@ let contactFormLink = document.getElementById('contact-form-link');
 contactFormLink.onclick = function link() {
     document.getElementById('contact-form').scrollIntoView({behavior: 'smooth'});
   }
+
+
+
+/*блок сервисы*/
+
+const servicesBlockContentInfo = document.getElementById("services-block-content-info");
+
+const arr = [{
+  titleService: "Research",
+  descriptionService: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla illo architecto aspernatur quod. Dolores!"
+},
+{
+  titleService: "Portfolio",
+  descriptionService: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla illo architecto aspernatur quod. Dolores!"
+},
+
+{
+  titleService: "Support",
+  descriptionService: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla illo architecto aspernatur quod. Dolores!"
+},
+
+{
+  titleService: "Documentation",
+  descriptionService: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla illo architecto aspernatur quod. Dolores!"
+},
+
+{
+  titleService: "Development",
+  descriptionService: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla illo architecto aspernatur quod. Dolores!"
+},
+
+{
+  titleService: "Design",
+  descriptionService: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla illo architecto aspernatur quod. Dolores!"
+}
+];
+
+arr.forEach((item) => {
+  createElement(item.titleService, item.descriptionService);
+});
+
+function createElement(title, description){
+  const serviceBlock = document.createElement('div');
+  serviceBlock.className = "servicesBlockContent";
+  const titleService = document.createElement("h1");
+  const descriptionService = document.createElement("p");
+  servicesBlockContent.appendChild(serviceBlock);
+  serviceBlock.append(title);
+  serviceBlock.append(description);
+  titleService.innerHTML = title;
+  descriptionService.innerHTML = description;
+}
+
+//css
+
+
+
+
+
 debugger
