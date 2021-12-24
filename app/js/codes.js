64e9d04e0448c7b9421e90571138c532081f1aa3
Naimeless <65905724+Naimeless@users.noreply.github.com>
2021-12-24 17:35:29 +0300

    стили JS
    прописывание стилей для блока service при помощи JS

--- app/js/codes.js
+++ app/js/codes.js
@@ -8,7 +8,7 @@ contactFormLink.onclick = function link() {
 
 /*блок сервисы*/
 
-const servicesBlockContentInfo = document.getElementById("services-block-content-info");
+const servicesBlockContent = document.getElementById("services-block-content");
 
 const arr = [{
   titleService: "Research",
@@ -46,20 +46,29 @@ arr.forEach((item) => {
 
 function createElement(title, description){
   const serviceBlock = document.createElement('div');
-  serviceBlock.className = "servicesBlockContent";
+  serviceBlock.className = "services-block__content-info";
+  //заголовок
   const titleService = document.createElement("h1");
+  titleService.className = "services-block__content-info-text";
+  //текст
   const descriptionService = document.createElement("p");
+  descriptionService.className = "services-block__content-info-text";
+  //кнопка
+  const btnService = document.createElement("button");
+  btnService.className = "services-block__content-info-btn";
+  btnService.textContent='READ MORE';
+  //картинка
+  const imgService = document.createElement("img");
+  imgService.className = "services-block__content-info-icon";
+  imgService.src = "img/smartphone_copy.png";
+  //добавление
   servicesBlockContent.appendChild(serviceBlock);
-  serviceBlock.append(title);
-  serviceBlock.append(description);
+  serviceBlock.append(imgService);
+  serviceBlock.append(titleService);
+  serviceBlock.append(descriptionService);
+  serviceBlock.append(btnService);
   titleService.innerHTML = title;
   descriptionService.innerHTML = description;
 }
-
-//css
-
-
-
-
 
 debugger
